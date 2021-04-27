0c7f6abfa63b2efc65036419548df7cb7c1fe184
lee <sunmin.lee01@gmail.com>
2021-04-28 00:35:40 +0900

    VariableTest 자료

---
+++ src/com/kosta/VariableTest.java
@@ -0,0 +1,16 @@
+package com.kosta;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         byte a=10;
+         byte b=20;
+         //byte c=a+b;  ¿¡·¯ 
+         
+         int c=a+b;
+         
+         
+	}
+
+}
